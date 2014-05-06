f2b556b3c715788025a07b0a67e14a84b3c43c00
Hyperion <gabriel.violet.dream@gmail.com>
2014-05-06 20:56:24 +0300

    Some code cleanup

--- main.cpp
+++ main.cpp
@@ -1,25 +1,26 @@
+// Copyright 2014 <Artem Korneev>
+
+#include <unistd.h>
+#include <dirent.h>
+#include <StormLib.h>
 #include <iostream>
 #include <string>
 #include <vector>
+#include <set>
 #include <algorithm>
-#include <list>
-#include <unistd.h>
-#include <dirent.h>
+#include <functional>
 #include <cerrno>
-#include <StormLib.h>
 
-using namespace std;
-
-typedef struct _FileEntry {
-    string  filename;
-    string  fullPath;
+struct FileEntry {
+    std::string  filename;
+    std::string  fullPath;
     DWORD   fileSize;
     DWORD   compressedSize;
-    //DWORD   lowDateTime;
-    //DWORD   highDateTime;
-    //LCID    locale;
+    // DWORD   lowDateTime;
+    // DWORD   highDateTime;
+    // LCID    locale;
     bool    isDir;
-} FileEntry;
+};
 
 enum MODE {
     EXTRACT,
@@ -28,34 +29,37 @@ enum MODE {
     DELETE,
 };
 
-string filename;
+std::string filename;
 HANDLE archive;
-vector<FileEntry> files;
-vector<FileEntry> directories;
+std::vector<FileEntry> files;
+std::vector<FileEntry> directories;
 
 DWORD maxCompressedSize = 0;
 DWORD maxFileSize = 0;
 
-void printError(string message)
+void printError(std::string message)
 {
     int nError = GetLastError();
 
     message = "mpq: " + message;
 
-    switch(nError) {
-        case ERROR_BAD_FORMAT:
-            cerr << message << ": Bad format" << endl;
-            break;
-        case ERROR_FILE_CORRUPT:
-            cerr << message << ": File corrupted" << endl;
-            break;
-        default:
-            perror(message.c_str());
-            break;
+    switch (nError) {
+    case ERROR_BAD_FORMAT:
+        std::cerr << message << ": Bad format" << "\n";
+        break;
+    case ERROR_NO_MORE_FILES:
+        std::cerr << message << ": No more files" << "\n";
+        break;
+    case ERROR_FILE_CORRUPT:
+        std::cerr << message << ": File corrupted" << "\n";
+        break;
+    default:
+        perror(message.c_str());
+        break;
     }
 }
 
-int intLenght(DWORD num)
+int intLength(DWORD num)
 {
     int i = 0;
     do {
@@ -65,10 +69,10 @@ int intLenght(DWORD num)
     return i;
 }
 
-bool listFiles(string pattern)
+bool listFiles(std::string pattern)
 {
     SFILE_FIND_DATA findData;
-    list<string> directoryNames;
+    std::set<std::string, std::less<std::string> > directoryNames;
     HANDLE handle = SFileFindFirstFile(archive, pattern.c_str(), &findData, 0);
 
     if (handle) {
@@ -79,13 +83,13 @@ bool listFiles(string pattern)
             e.fullPath          = findData.cFileName;
             e.compressedSize    = findData.dwCompSize;
             e.fileSize          = findData.dwFileSize;
-            //e.highDateTime      = findData.dwFileTimeHi;
-            //e.lowDateTime       = findData.dwFileTimeLo;
-            //e.locale            = findData.lcLocale;
+            // e.highDateTime      = findData.dwFileTimeHi;
+            // e.lowDateTime       = findData.dwFileTimeLo;
+            // e.locale            = findData.lcLocale;
             e.isDir = false;
 
-            //TODO: check the platform
-            replace(e.fullPath.begin(), e.fullPath.end(), '\\', '/');
+            // TODO(hyperion): check the platform.
+            std::replace(e.fullPath.begin(), e.fullPath.end(), '\\', '/');
 
             if (e.compressedSize > maxCompressedSize) {
                 maxCompressedSize = e.compressedSize;
@@ -96,38 +100,37 @@ bool listFiles(string pattern)
 
             files.push_back(e);
 
-            //Adding directories
-            string directoriesChain;
+            // Adding directories
+            size_t offset = e.fullPath.rfind('/');
+            if (offset != std::string::npos) {
+                std::string filePath = e.fullPath.substr(0, offset + 1);
+                size_t start = filePath.find('/', 0);
 
-            for (size_t offset = e.fullPath.find('/'), start = 0; offset != string::npos; start = offset + 1, offset = e.fullPath.find('/', start)) {
-                directoriesChain += e.fullPath.substr(start, offset - start) + "/";
-                directoryNames.push_back(directoriesChain);
+                while (start != std::string::npos) {
+                    directoryNames.insert(filePath.substr(0, start + 1));
+                    start = filePath.find('/', start + 1);
+                }
             }
-
         } while (SFileFindNextFile(handle, &findData));
 
         SFileFindClose(handle);
     } else {
-        printError("error finding pattern " + pattern);
+        printError("error finding pattern '" + pattern + "'");
         return false;
     }
 
-    maxCompressedSize = intLenght(maxCompressedSize);
-    maxFileSize = intLenght(maxFileSize);
+    maxCompressedSize = intLength(maxCompressedSize);
+    maxFileSize = intLength(maxFileSize);
 
-    directoryNames.sort();
-    directoryNames.unique();
-
-    list<string>::iterator iter, iterEnd;
-
-    for (iter = directoryNames.begin(), iterEnd = directoryNames.end(); iter != iterEnd; ++iter) {
-        string fullPath = *iter;
-        string tmpDirectoryName = fullPath.substr(0, fullPath.length() - 1);
-        size_t offset = tmpDirectoryName.rfind('/');
+    std::set<std::string>::iterator iter;
+    for (iter = directoryNames.begin(); iter != directoryNames.end(); ++iter) {
+        std::string fullPath = *iter;
+        std::string dirName = fullPath.substr(0, fullPath.length() - 1);
+        size_t offset = dirName.rfind('/');
 
         FileEntry e;
 
-        e.filename = (offset != string::npos) ? tmpDirectoryName.substr(offset + 1) : tmpDirectoryName;
+        e.filename = (offset != std::string::npos) ? dirName.substr(offset + 1) : dirName;
         e.fullPath = fullPath;
         e.compressedSize = 0;
         e.fileSize = 0;
@@ -139,34 +142,38 @@ bool listFiles(string pattern)
     return true;
 }
 
+void printEntry(std::vector<FileEntry>::iterator entry)
+{
+    std::cout.width(maxFileSize);
+    std::cout << entry->fileSize << " ";
+    std::cout.width(maxCompressedSize);
+    std::cout << entry->compressedSize << " ";
+    std::cout << entry->fullPath << "\n";
+}
+
 void printList()
 {
-    vector<FileEntry> filesAndDirectories = files;
-    filesAndDirectories.insert(filesAndDirectories.end(), directories.begin(), directories.end());
+    std::vector<FileEntry>::iterator iter;
+    for (iter = files.begin(); iter != files.end(); ++iter) {
+        printEntry(iter);
+    }
 
-    vector<FileEntry>::iterator iter, iterEnd;
-    for (iter = filesAndDirectories.begin(), iterEnd = filesAndDirectories.end(); iter != iterEnd; ++iter) {
-        cout.width(maxFileSize);
-        cout << iter->fileSize << " ";
-        cout.width(maxCompressedSize);
-        cout << iter->compressedSize << " ";
-        cout << iter->fullPath << endl;
+    for (iter = directories.begin(); iter != directories.end(); ++iter) {
+        printEntry(iter);
     }
 }
 
-bool extractFile(vector<FileEntry>::iterator entry, string destinationDirectory)
+bool extractFile(std::vector<FileEntry>::iterator entry, std::string destinationFile)
 {
-    string destinationFile = destinationDirectory;
     destinationFile += entry->fullPath;
 
-    size_t offset;
-    offset = destinationFile.find_last_of("/");
-    if (offset != string::npos) {
-        string dest = destinationFile.substr(0, offset + 1);
+    size_t offset = destinationFile.rfind('/');
+    if (offset != std::string::npos) {
+        std::string dest = destinationFile.substr(0, offset + 1);
 
-        size_t start = dest.find("/", 0);
-        while (start != string::npos) {
-            string dirname = dest.substr(0, start);
+        size_t start = dest.find('/', 0);
+        while (start != std::string::npos) {
+            std::string dirname = dest.substr(0, start);
 
             DIR* d = opendir(dirname.c_str());
             if (!d)
@@ -174,27 +181,27 @@ bool extractFile(vector<FileEntry>::iterator entry, string destinationDirectory)
             else
                 closedir(d);
 
-            start = dest.find("/", start + 1);
+            start = dest.find('/', start + 1);
         }
     }
 
     return SFileExtractFile(archive, entry->fullPath.c_str(), destinationFile.c_str(), 0);
 }
 
-void extractFiles(string destinationDirectory)
+void extractFiles(std::string destinationDirectory)
 {
     if (!files.empty()) {
-        cout << endl;
-        cout << "Extracting files..." << endl;
-        cout << endl;
+        std::cout << "\n";
+        std::cout << "Extracting files..." << "\n";
+        std::cout << "\n";
 
         if (destinationDirectory.at(destinationDirectory.length() - 1) != '/')
             destinationDirectory += "/";
 
-        vector<FileEntry>::iterator iter, iterEnd;
-        for (iter = files.begin(), iterEnd = files.end(); iter != iterEnd; ++iter) {
+        std::vector<FileEntry>::iterator iter;
+        for (iter = files.begin(); iter != files.end(); ++iter) {
             if (extractFile(iter, destinationDirectory))
-                cout << iter->fullPath << endl;
+                std::cout << iter->fullPath << "\n";
             else
                 printError("failed to extract the file '" + iter->fullPath + "'");
         }
@@ -205,11 +212,9 @@ void extractFiles(string destinationDirectory)
 int main(int argc, char** argv)
 {
     MODE mode = EXTRACT;
-    string pattern = "*";
-    string destinationDirectory = ".";
+    std::string destinationDirectory = ".";
 
     int option;
-    bool listResult = false;
 
     while ((option = getopt(argc, argv, "d:hZ")) != -1)
         switch (option) {
@@ -221,7 +226,7 @@ int main(int argc, char** argv)
             break;
         case 'h':
         default:
-            cout << "TODO: Add help page" << endl;
+            std::cout << "TODO: Add help page" << "\n";
             exit(EXIT_SUCCESS);
         }
 
@@ -232,7 +237,10 @@ int main(int argc, char** argv)
         exit(EXIT_FAILURE);
     }
 
-    cout << "Archive: " << filename << endl;
+    std::cout << "Archive: " << filename << "\n";
+
+    std::string pattern = "*";
+    bool listResult = false;
 
     switch (mode) {
     case LIST:
@@ -244,7 +252,7 @@ int main(int argc, char** argv)
     case EXTRACT:
         if (argc > optind + 1)
             for (int i = optind + 1; i < argc; ++i)
-                listResult = listFiles(string(argv[i]));
+                listResult = listFiles(std::string(argv[i]));
         else
             listResult = listFiles(pattern);
 
@@ -262,4 +270,4 @@ int main(int argc, char** argv)
 
     exit(EXIT_SUCCESS);
 }
-// kate: indent-mode cstyle; indent-width 4; replace-tabs on; 
+// kate: indent-mode cstyle; indent-width 4; replace-tabs on;
